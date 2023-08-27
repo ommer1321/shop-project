d66843cb8fa595273c5c6c7a9c1c0091281c8266
ommer1321 <1453o.faruk@gmail.com>
2023-08-27 17:42:40 +0300

    ders - 2

---
+++ data.js
@@ -0,0 +1,23 @@
+module.exports = function () {
+  return {
+    products: [
+      { id: 1, name: "Samsung s1", price: 100, imageUrl: "1.jpg" },
+      { id: 2, name: "Samsung s2", price: 100, imageUrl: "2.jpg" },
+      { id: 3, name: "Samsung s3", price: 100, imageUrl: "3.jpg" },
+      { id: 4, name: "Samsung s4", price: 100, imageUrl: "4.jpg" },
+      { id: 5, name: "Samsung s5", price: 100, imageUrl: "5.jpg" },
+      { id: 6, name: "Samsung s6", price: 100, imageUrl: "6.jpg" },
+      { id: 7, name: "Samsung s7", price: 100, imageUrl: "7.jpg" },
+    ],
+
+    categories: [
+      { id: 1, name: "Bilgisayar" },
+      { id: 2, name: "Araba" },
+      { id: 3, name: "Oyuncak" },
+      { id: 4, name: "Ev Aletleri" },
+      { id: 5, name: "Samsung s5" },
+    ],
+
+    orders: [],
+  };
+};
