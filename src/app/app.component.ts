24d20d1f5053a7b36243c2239b232562df981154
ommer1321 <1453o.faruk@gmail.com>
2023-08-27 17:21:18 +0300

    kurulum

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'root',
+  template:`
+  <div class="btn btn-danger">asd</div>
+  
+
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'shop-project';
+}
