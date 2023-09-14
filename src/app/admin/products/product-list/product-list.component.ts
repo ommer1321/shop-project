81e6acc10d25670f6000df23b19379c833285e3c
ommer1321 <1453o.faruk@gmail.com>
2023-09-14 17:31:44 +0300

    ders - 29

--- src/app/admin/products/product-list/product-list.component.ts
+++ src/app/admin/products/product-list/product-list.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/model/product.model';
+import { ProductRepository } from 'src/app/model/product.reporsitory';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
 })
 export class ProductListComponent {
+  constructor(public productRepository:ProductRepository){}
+
+  getProducts() : Product[]{
+
+    return this.productRepository.getProducts()
+
+  }
 
 }
